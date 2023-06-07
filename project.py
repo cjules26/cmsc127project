ea9479c0342567a903524176fe0a852ec5cc983c
cjules26 <caandrada@up.edu.ph>
2023-06-07 23:15:37 +0800

    chore: added menu, user menu and add user but add user still needs fix

--- project.py
+++ project.py
@@ -1,16 +1,14 @@
 import mysql.connector as mariadb 
 
-mariadb_connection = mariadb.connect(user ='root', password ='mariadb26', host='localhost', port='3306')
+mariadb_connection = mariadb.connect(user ='root', password ='mariadb26', database = 'app',host='localhost', port='3306')
 
-create_cursor =mariadb_connection.cursor()
+create_cursor = mariadb_connection.cursor(buffered =True)
 
 create_cursor.execute("DROP DATABASE IF EXISTS `app`")
+
 create_cursor.execute("CREATE DATABASE IF NOT EXISTS `app`")
 
 create_cursor.execute("SHOW DATABASES")
-
-for x in create_cursor:
-    print(x)
 
 create_cursor.execute("USE app")
 
@@ -22,5 +20,62 @@ create_cursor.execute("CREATE TABLE GROUP_MEMBER(groupID VARCHAR(4) NOT NULL, me
 
 create_cursor.execute("CREATE TABLE EXPENSE(expenseID VARCHAR(6) NOT NULL, amount DECIMAL(8,2), sender VARCHAR(4), recipient VARCHAR(4), dateOwed DATE, datePaid DATE, userID VARCHAR(4), groupID VARCHAR(4), PRIMARY KEY(expenseID), CONSTRAINT deptfk FOREIGN KEY (userID) references PERSON(userID), CONSTRAINT groupfk FOREIGN KEY (groupID) references GROUPING(groupID))")
 
+def addUser():
+    userIDcount = create_cursor.execute("Select count(userID) from person")
+    if userIDcount == None:
+        userID = "U1"
+    else:
+        userID = "U"+str(userIDcount)
+    fName = input("Enter First Name: ")
+    lName = input("Enter Last Name: ")
+    moneyOwed = float(input("Enter Money Owed: "))
+    moneyLent = float(input("Enter Money Lent: "))
+    # borrowerId_fk = input("Enter Borrower id:")
+    sql_statement = 'INSERT INTO PERSON(userId,fName,lName,moneyOwed,moneyLent) VALUES(%s,%s,%s,%s,%s)'
+    insert = (userID,fName,lName,moneyOwed,moneyLent)
+    create_cursor.execute(sql_statement, insert)
+    mariadb_connection.commit()
 
-name = input("What is your name?")
+def userMenu():
+    choice = -1
+    while choice!=0:
+        print("\n**********USER MENU*********")
+        print("[1] Add User")
+        print("[2] Update User")
+        print("[3] Delete User")
+        print("[0] Return\n")
+        choice = input("Please enter choice: ")
+        if choice == "1":
+            addUser()
+        elif choice == "2":
+            print("2")
+        elif choice == "3":
+            print("3")
+        elif choice == "0":
+            break
+        else:
+            print("Invalid Choice!!!")
+
+def menu():
+    choice = -1
+    while (choice!=0):
+        print("**********MENU*********")
+        print("[1] User")
+        print("[2] Group")
+        print("[3] Expense")
+        print("[0] Exit\n")
+        choice = input("Please enter choice: ")
+        if choice == "1":
+            userMenu()
+        elif choice == "2":
+            print("2")
+        elif choice == "3":
+            print("3")
+        elif choice == "0":
+            print("Thank you!")
+            break
+        else:
+            print("Invalid Choice!!!")
+
+
+menu()
